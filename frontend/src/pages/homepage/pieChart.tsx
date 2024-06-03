659f40095f13b4cc83d23c8aaa5c001329817356
cd011 <zerocj00@gmail.com>
2024-06-03 21:32:08 +0530

    pie

---
+++ frontend/src/pages/homepage/pieChart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+
+export const pieChart = () => {
+  const [showChats, setShowChats] = useState(false);
+  return <div>pieChart</div>;
+};
